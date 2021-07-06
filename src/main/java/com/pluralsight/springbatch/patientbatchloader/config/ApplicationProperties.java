352dd363312c56a509f7c43d24cf332b80a3698d
kunalmnnit <ksevkani495@gmail.com>
2021-07-06 11:19:53 +0530

    Initial commit

---
+++ src/main/java/com/pluralsight/springbatch/patientbatchloader/config/ApplicationProperties.java
@@ -0,0 +1,26 @@
+package com.pluralsight.springbatch.patientbatchloader.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Properties specific to Patient Batch Loader.
+ * <p>
+ * Properties are configured in the application.yml file.
+ */
+@ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+public class ApplicationProperties {
+    private final Batch batch = new Batch();
+    public Batch getBatch() {
+        return batch;
+    }
+    public static class Batch {
+        private String inputPath = "C:/Users/admin/OneDrive - MNNIT Allahabad, Prayagraj, India/Documents/patient-batch-loader-start/data";
+
+        public void setInputPath(String inputPath) {
+            this.inputPath = inputPath;
+        }
+        public String  getInputPath() {
+            return this.inputPath;
+        }
+    }
+}
